7646c1e7adef3e2191dc6a058282b0c071c25e87
Iurovet <s4003879@student.rmit.edu.au>
2025-01-27 07:30:13 +1100

    Update key format for non-jokers

--- Rummikub.java
+++ Rummikub.java
@@ -4,14 +4,14 @@ public class Rummikub {
     public static HashMap<String, Tile> setupTiles() {
         HashMap<String, Tile> tiles = new HashMap<String, Tile>();
 
-        final String[] colourList = {"Blue", "Orange", "Red", "Black"};
+        final String[] colours = {"Blue", "Orange", "Red", "Black"};
         final String[] identifiers = {"a", "b"};
 
-        for (String s1 : colourList) {
+        for (String s1: colours) {
             for (int i = 1; i <= 13; i++) {
                 for (String s2 : identifiers) {
                     Tile t1 = new Tile(s1, "non-joker", i, s2);
-                    tiles.put(s1.substring(0, 1) + " " + i + " " + s2, t1);
+                    tiles.put(s1 + " " + i + s2, t1);
                 }
             }
         }
