44e89cc2335ed2d571c99584bba48966c8bb8abb
Iurovet <s4003879@student.rmit.edu.au>
2025-06-19 15:31:10 +1000

    Add logic to get the number of players (and starting player, including by random choice),

--- Rummikub.py
+++ Rummikub.py
@@ -1,3 +1,5 @@
+import random
+
 class ErrorMessage: # Use to easily print out all error messages along with their associated titles.
     def __init__(self):
         self.text = ""
@@ -32,6 +34,42 @@ class Tile:
     def getNumber(self):
         return self.number
 
+def getNumPlayers():
+    numPlayers = None
+    
+    try:
+        numPlayers = int(input("Please enter the number of players (2-4)\n"))
+    except Exception as e: # Wrong type
+        numPlayers = 0 # Ensure wrong input doesn't get treated as valid
+    
+    return numPlayers
+
+def getStartPlayer():
+    startPlayer = None
+    
+    try:
+        startPlayer = int(input("Please choose the player number (or 5 for random)\n"))
+    except Exception as e: # Wrong type
+        startPlayer = 0 # Ensure wrong input doesn't get treated as valid
+    
+    return startPlayer
+
 if __name__ == '__main__': # Main method
-        
-    pass
+    print("Welcome to Rummikub!", end = " ")
+    
+    # Get number of players
+    numPlayers = getNumPlayers()
+    while (numPlayers < 2 or numPlayers > 4):
+        print("Error: Number of players must be between 2 and 4.", end = " ")
+        numPlayers = getNumPlayers()
+    
+    # Choose explicitly or randomly
+    startPlayer = getStartPlayer()
+    while (startPlayer < 1 or (startPlayer > numPlayers and startPlayer != 5)):
+        print("Error: Player number must be between 1 and " + str(numPlayers) + " (or 5 for random)", end = " ")
+        startPlayer = getStartPlayer()
+    
+    # Randomly assign starting player (if necessary) and print out the final decision
+    if startPlayer == 5:
+        startPlayer = random.randint(1, numPlayers)
+    print("Player", startPlayer, "is starting")
