9bfbdf69572228e8d1d564cfcf60df3e68b77958
Iurovet <s4003879@student.rmit.edu.au>
2025-06-19 16:41:35 +1000

    Add assignment of tiles.

--- Rummikub.py
+++ Rummikub.py
@@ -1,4 +1,4 @@
-import random
+import math, random
 
 class ErrorMessage: # Use to easily print out all error messages along with their associated titles.
     def __init__(self):
@@ -9,20 +9,16 @@ class ErrorMessage: # Use to easily print out all error messages along with thei
         return "Error: " + self.text + " for the following tiles: " + self.associatedTiles
 
 class Tile:
-    def __init__(self, colour): # Jokers only
-        self.colour = colour # Red or black (no effect on other tile colours)
-        self.number = 0
-
-    def __init__(self, colour, number): # Non-jokers only
-        self.colour = colour # Blue, orange, red or black
-        self.number = number # Possible values are in the range [1, 13]
+    def __init__(self, colour, number=None): # Only non-jokers will have the 2nd parameter (cannot overload constructors)
+        self.colour = colour # Blue, orange, red or black (the latter 2 for jokers only)
+        self.number = number if number is not None else 0 # [1, 13] for the former case
     
     def __str__(self): # Print the object
         match self.number:
             case 0:
-                return self.colour + " joker"
+                return str(self.colour) + " joker"
             case _: # Default case
-                return self.colour + " " + self.number
+                return str(self.colour) + " " + str(self.number)
             
     def setColour(self, colour):
         self.colour = colour
@@ -33,6 +29,35 @@ class Tile:
         self.number = number
     def getNumber(self):
         return self.number
+    
+def allocateTiles(numPlayers):
+    pool, player1Tiles, player2Tiles, player3Tiles, player4Tiles = [], [], [], [], []
+    
+    # Add non-jokers
+    for i in range (1, 13+1): # Tile numbers are in range [1, 13] but end index is exclusive
+        for c1 in ["Blue", "Orange", "Black", "Red"]:
+            for j in range (2):
+                pool.append(Tile(c1, i))
+
+    # Add jokers
+    pool.append(Tile("Black"))
+    pool.append(Tile("Red"))
+
+    for i in range (0, 14 * numPlayers): # Add 14 tiles to each player by random assignment
+        newTile = pool.pop(random.randint(0, len(pool) - 1)) # Analogous to Ctrl+X
+        
+        # By cycling between the number of tiles (as opposed to that of the players), we avoid assigning tiles to extra players (and potentially in an uneven fashion)
+        match math.floor(i / 14):
+            case 0:
+                player1Tiles.append(newTile)
+            case 1:
+                player2Tiles.append(newTile)
+            case 2:
+                player3Tiles.append(newTile)
+            case 3:
+                player4Tiles.append(newTile)
+
+    return pool, player1Tiles, player2Tiles, player3Tiles, player4Tiles
 
 def getNumPlayers():
     numPlayers = None
@@ -63,7 +88,7 @@ if __name__ == '__main__': # Main method
         print("Error: Number of players must be between 2 and 4.", end = " ")
         numPlayers = getNumPlayers()
     
-    # Choose explicitly or randomly
+    # Choose the starting player explicitly or randomly
     startPlayer = getStartPlayer()
     while (startPlayer < 1 or (startPlayer > numPlayers and startPlayer != 5)):
         print("Error: Player number must be between 1 and " + str(numPlayers) + " (or 5 for random)", end = " ")
@@ -73,3 +98,9 @@ if __name__ == '__main__': # Main method
     if startPlayer == 5:
         startPlayer = random.randint(1, numPlayers)
     print("Player", startPlayer, "is starting")
+
+    pool, player1Tiles, player2Tiles, player3Tiles, player4Tiles = allocateTiles(numPlayers)
+    if numPlayers < 4: # The respective lists should be empty anyway, but destroy them just in case
+        del player4Tiles
+        if numPlayers < 3:
+            del player3Tiles
